fb3f96311b5194ae2d9ac8059c21b4fa7c58059c
Bekzod Sharipbaev <python.bekzod@gmail.com>
2024-01-29 18:24:24 +0500

    first commit

---
+++ src/components/Header/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="header-main w-full flex justify-evenly p-3">
+    <div class="left-sect hover:cursor-pointer">
+      <a href="#"></a>
+      <img class="w-44 object-cover	object-center" src="/src/assets/media/logo.png" alt="logo" />
+    </div>
+    <div class="middle-sect flex gap-5">
+      <div class="email flex items-center gap-3">
+        <i class="fa-solid fa-envelope icons text-green-600"></i>
+        <p class="text-lg">info@uzlogistic.uz</p>
+      </div>
+      <div class="phone flex items-center gap-3">
+        <i class="fa-solid fa-phone icons text-green-600"></i>
+        <p class="font-bold">(+998 78) 150-80-08</p>
+      </div>
+    </div>
+    <div class="">
+
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.header-main {
+  background-color: rgb(11, 41, 79);
+  color: white;
+  font-family: 'Oswald', sans-serif;
+}
+
+.icons {
+  font-size: 22px;
+}
+</style>
