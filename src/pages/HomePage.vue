83a31f55103c48f806ffe9488a90be14a0b37c25
Bekzod Sharipbaev <python.bekzod@gmail.com>
2024-02-01 15:59:29 +0500

    'second-section'

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,44 +1,64 @@
 <template>
-  <div class="first-sect pt-20 text-white">
-    <div class="container txt-log text-left w-full">
-      <h1
-        class="text-[30px] w-[250px] lit:w-[400px] sm:text-[40px] sm:w-[550px] lg:text-[70px] lg:w-[900px] mb-4"
-      >
-        Комплексная технологичная ЛОГИСТИКА
-      </h1>
-      <p class="w-[240px] lit:w-[400px] sm:w-[550px] lg:w-[630px]">
-        Компания СП ООО «Uzlogistic» за короткое время успела стать динамично развивающимся
-        3PL-оператором, предоставляющим полный спектр логистических услуг. Мы работаем с любыми
-        типами грузов и организовываем поставки из любой точки мира под различными таможенными
-        процедурами. Ключевым преимуществом компаний является собственная логистическая
-        инфраструктура, а также то, что СП ООО «Uzlogistic» включена в реестр таможенных
-        перевозчиков, осуществляющих перевозку товаров, находящихся под таможенным контролем без
-        таможенного сопровождения, и в реестр единых поставщиков услуг, что позволяет заключать
-        прямые договоры с местными организациями, осуществляющими внешнеторговые операции, без
-        проведения тендера.
-      </p>
+  <div class="main-div">
+    <div class="first-sect pt-20 text-white">
+      <div class="container txt-log text-left w-full">
+        <h1
+          class="text-[30px] w-[250px] lit:w-[400px] sm:text-[40px] sm:w-[550px] lg:text-[70px] lg:w-[900px] mb-4"
+        >
+          Комплексная технологичная ЛОГИСТИКА
+        </h1>
+        <p class="w-[240px] lit:w-[400px] sm:w-[550px] lg:w-[630px]">
+          Компания СП ООО «Uzlogistic» за короткое время успела стать динамично развивающимся
+          3PL-оператором, предоставляющим полный спектр логистических услуг. Мы работаем с любыми
+          типами грузов и организовываем поставки из любой точки мира под различными таможенными
+          процедурами. Ключевым преимуществом компаний является собственная логистическая
+          инфраструктура, а также то, что СП ООО «Uzlogistic» включена в реестр таможенных
+          перевозчиков, осуществляющих перевозку товаров, находящихся под таможенным контролем без
+          таможенного сопровождения, и в реестр единых поставщиков услуг, что позволяет заключать
+          прямые договоры с местными организациями, осуществляющими внешнеторговые операции, без
+          проведения тендера.
+        </p>
+      </div>
+      <div class="containerr pt-16 pl-8">
+        <div class="grid grid-cols-2 gap-2 sm:grid-cols-3 lg:grid lg:grid-cols-5 lg:gap-5">
+          <div class="grid lg:flex items-center gap-3">
+            <img class="w-12" src="/public/icons/advantage-1.png" alt="" />
+            <p class="w-[130px] text-sm font-normal">Професиональная команда</p>
+          </div>
+          <div class="grid lg:flex items-center gap-3">
+            <img class="w-12" src="/public/icons/advantage-2.png" alt="" />
+            <p class="w-[150px] text-sm font-normal">Обширная география перевозок</p>
+          </div>
+          <div class="grid lg:flex items-center gap-3">
+            <img class="w-12" src="/public/icons/advantage-3.png" alt="" />
+            <p class="w-[100px] text-sm font-normal">58 стран</p>
+          </div>
+          <div class="grid lg:flex items-center gap-3">
+            <img class="w-12" src="/public/icons/advantage-4.png" alt="" />
+            <p class="w-[170px] text-sm font-normal">Более 554 автомобилей</p>
+          </div>
+          <div class="grid lg:flex items-center gap-3">
+            <img class="w-12" src="/public/icons/advantage-5.png" alt="" />
+            <p class="w-[130px] text-sm font-normal">Собственный терминал</p>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="containerr pt-16 pl-8">
-      <div class="flex gap-5">
-        <div class="flex items-center gap-3">
-          <img class="w-12" src="/public/icons/advantage-1.png" alt="" />
-          <p class="w-[130px] text-sm font-normal">Професиональная команда</p>
+    <div class="second-sect z-1 flex items-center">
+      <div class="about-block z-20 w-6/12 h-[420px] pt-12 pl-[350px]">
+        <div class="title text-6xl pb-20 relative w-[500px]">
+          <h1>
+            О нашей компании
+            <span class="absolute right-0 top-[40px] text-green-600 font-medium">Uzlogistic</span>
+          </h1>
         </div>
-        <div class="flex items-center gap-3">
-          <img class="w-12" src="/public/icons/advantage-2.png" alt="" />
-          <p class="w-[150px] text-sm font-normal">Обширная география перевозок</p>
-        </div>
-        <div class="flex items-center gap-3">
-          <img class="w-12" src="/public/icons/advantage-3.png" alt="" />
-          <p class="w-[100px] text-sm font-normal">58 стран</p>
-        </div>
-        <div class="flex items-center gap-3">
-          <img class="w-12" src="/public/icons/advantage-4.png" alt="" />
-          <p class="w-[170px] text-sm font-normal">Более 554 автомобилей</p>
-        </div>
-        <div class="flex items-center gap-3">
-          <img class="w-12" src="/public/icons/advantage-5.png" alt="" />
-          <p class="w-[130px] text-sm font-normal">Собственный терминал</p>
+        <div class="text-content w-[520px]">
+          <p>
+            Основными направлениями деятельности являются интермодальные и мультимодальные перевозки
+            всеми видами транспорта, а также складские и терминальные услуги внутри и за пределами
+            Республики Узбекистан. При этом компания является основным перевозчиком
+            машиностроительной отрасли страны, в частности, АО «UzAuto Motors».
+          </p>
         </div>
       </div>
     </div>
@@ -48,13 +68,27 @@
 <script setup></script>
 
 <style scoped>
+.main-div {
+}
 .first-sect {
   background-image: url('public/slider/slide-1.jpg');
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
-  min-height: calc(100svh - 141px);
   font-family: 'Oswald', sans-serif;
+  min-height: calc(100svh - 141px);
+}
+
+.second-sect {
+  background-image: url('public/bg/bg-about.jpg');
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  min-height: calc(100svh);
+}
+
+.about-block {
+  box-shadow: 8px 5px 100px 10px rgb(189, 188, 188);
 }
 
 .containerr {
