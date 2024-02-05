df6c6a4ca7c62b3d70774f742793fb1a3ba4b245
Bekzod Sharipbaev <python.bekzod@gmail.com>
2024-02-05 18:06:18 +0500

    'main-page-process'

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="wrapper">
     <div class="first-sect">
-      <div class="container flex flex-col items-center justify-center h-screen">
-        <div class="content">
-          <h1 class="text-7xl font-normal w-4/5 mb-3.5 leading-normal">
+      <div
+        class="container flex flex-col items-center justify-center h-screen max_min:justify-normal"
+      >
+        <div class="content pb-32">
+          <h1
+            class="text-7xl font-normal w-4/5 mb-3.5 leading-normal max_lg:leading-none max_lg:w-full max_md:text-5xl max_md:text-center max_min:text-3xl max_lit:text-2xl"
+          >
             Комплексная технологичная ЛОГИСТИКА
           </h1>
-          <p class="w-2/4 leading-normal">
+          <p class="w-2/4 leading-normal max_lg:w-4/5 max_md:text-center max_md:w-full">
             Компания СП ООО «Uzlogistic» за короткое время успела стать динамично развивающимся
             3PL-оператором, предоставляющим полный спектр логистических услуг. Мы работаем с любыми
             типами грузов и организовываем поставки из любой точки мира под различными таможенными
@@ -18,31 +22,96 @@
             проведения тендера.
           </p>
         </div>
-        <div class="advantages pt-32">
+        <div class="advantages">
           <div class="advantages-items">
-            <ul class="grid grid-cols-5 gap-2">
-              <li class="flex items-center gap-3">
+            <ul class="grid grid-cols-5 gap-2 max_lg:grid-cols-3 max_sm:grid-cols-2">
+              <li class="flex items-center gap-3 max_min:grid max_min:w-18">
                 <img src="/public/icons/advantage-1.png" alt="advantage-1" />
                 <p>Профессиональная команда</p>
               </li>
-              <li class="flex items-center gap-3">
+              <li class="flex items-center gap-3 max_min:grid max_min:w-18">
                 <img src="/public/icons/advantage-2.png" alt="advantage-1" />
                 <p>Обширная география перевозок</p>
               </li>
-              <li class="flex items-center gap-3">
+              <li class="flex items-center gap-3 max_min:grid max_min:w-18">
                 <img src="/public/icons/advantage-3.png" alt="advantage-1" />
                 <p>58 стран</p>
               </li>
-              <li class="flex items-center gap-3">
+              <li class="flex items-center gap-3 max_min:grid max_min:w-18">
                 <img src="/public/icons/advantage-4.png" alt="advantage-1" />
                 <p>Более 554 автомобилей</p>
               </li>
-              <li class="flex items-center gap-3">
+              <li class="flex items-center gap-3 max_min:grid max_min:w-18">
                 <img src="/public/icons/advantage-5.png" alt="advantage-1" />
                 <p>Собственный терминал</p>
               </li>
             </ul>
           </div>
+        </div>
+      </div>
+    </div>
+    <div class="advantages-small mt-3 hidden">
+      <div class="container">
+        <div class="advantages-items">
+          <ul class="grid grid-cols-5 gap-2 max_lg:grid-cols-3 max_sm:grid-cols-2">
+            <li class="flex items-center gap-3 max_min:grid max_min:w-18">
+              <img src="/public/icons/advantage-1.png" alt="advantage-1" />
+              <p>Профессиональная команда</p>
+            </li>
+            <li class="flex items-center gap-3 max_min:grid max_min:w-18">
+              <img src="/public/icons/advantage-2.png" alt="advantage-1" />
+              <p>Обширная география перевозок</p>
+            </li>
+            <li class="flex items-center gap-3 max_min:grid max_min:w-18">
+              <img src="/public/icons/advantage-3.png" alt="advantage-1" />
+              <p>58 стран</p>
+            </li>
+            <li class="flex items-center gap-3 max_min:grid max_min:w-18">
+              <img src="/public/icons/advantage-4.png" alt="advantage-1" />
+              <p>Более 554 автомобилей</p>
+            </li>
+            <li class="flex items-center gap-3 max_min:grid max_min:w-18">
+              <img src="/public/icons/advantage-5.png" alt="advantage-1" />
+              <p>Собственный терминал</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="second-sect flex items-center">
+      <div class="short-about w-1/2 h-[26rem] pt-14 pr-16">
+        <div class="about-content pl-96 max_2xl:pl-32 max_lg:pl-14">
+          <div class="about-title mb-20">
+            <h1 class="text-6xl text-black relative max_xl:text-4xl">
+              О нашей компании
+              <span class="absolute top-10 left-1/2 text-green-600">Uzlogistic</span>
+            </h1>
+          </div>
+          <div class="about-text">
+            <p class="text-gray-700 max_lg:text-xs">
+              Основными направлениями деятельности являются интермодальные и мультимодальные
+              перевозки всеми видами транспорта, а также складские и терминальные услуги внутри и за
+              пределами Республики Узбекистан. При этом компания является основным перевозчиком
+              машиностроительной отрасли страны, в частности, АО «UzAuto Motors».
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="services-sect">
+      <div class="container text-right">
+        <div class="services-title flex flex-col justify-items-end w-4/6">
+          <h1 class="text-6xl">
+            Наши основные
+            <b class="text-green-600">услуги</b>
+          </h1>
+          <p class="text-green-600">
+            Мы работаем с любыми типами грузов и организовываем поставки из любой точки мира под
+            различными таможенными процедурами. Гарантируем сохранность груза на всех этапах
+            перевозки и таможенного оформления. Мы смогли настроить операционную работу на
+            собственном специализированном терминале в г. Ангрен и на складах Кореи, Китая, Европы,
+            Северной и Южной Америки.
+          </p>
         </div>
       </div>
     </div>
@@ -125,7 +194,7 @@ p {
     linear-gradient(to right, rgba(2, 52, 109, 0.365), rgba(5, 61, 124, 0.605)) no-repeat center
       center fixed,
     url('/public/slider/slide-1.jpg') no-repeat center center fixed;
-  background-position: center;
+  background-size: cover;
   min-height: calc(100svh - 128px);
 }
 
@@ -138,10 +207,50 @@ p {
   background-repeat: no-repeat;
   background-size: contain;
   background-position: center;
-  min-height: calc(100svh);
+  min-height: 100svh;
 }
 
-.about-block {
+.advantages-small {
+  background-color: rgb(9, 32, 63);
+}
+
+.short-about {
   box-shadow: 8px 5px 100px 10px rgb(189, 188, 188);
+}
+
+.services-sect {
+  background-image: url('/public/bg/bg-services.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  min-height: 100svh;
+}
+
+@media (max-width: 768px) {
+  .advantages {
+    display: none;
+  }
+  .advantages-small {
+    display: block;
+  }
+  .second-sect {
+    min-height: 50svh;
+  }
+  .short-about {
+    box-shadow: none;
+    text-align: center;
+    width: 100%;
+    background:
+      linear-gradient(to right, rgba(2, 52, 109, 0.365), rgba(5, 61, 124, 0.605)) no-repeat center
+        center fixed,
+      url('/public/bg/bg-about.jpg') no-repeat center center fixed;
+  }
+  .about-text p {
+    font-size: 18px;
+  }
+
+  .about-title h1 span {
+    display: none;
+  }
 }
 </style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,18 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import HomePageVue from '@/pages/HomePage.vue'
-import temp from '@/pages/temp.vue'
 
 const routes = [
   {
     path: '/',
     component: HomePageVue,
     name: 'home'
-  },
-  {
-    path: '/temp',
-    component: temp,
-    name: 'temp'
   }
 ]
 const router = createRouter({
